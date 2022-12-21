3e44890dc55ffc76dd330a3429722775bab71c63
Theocode12 <vchidubem9@gmail.com>
2022-12-21 23:31:40 +0100

    use of promise.all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,17 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const profile = [];
-  uploadPhoto().then((result) => {
-    profile.push(result.body);
-  }).catch(() => {
-    console.log('Signup system offline');
-  });
-  createUser().then((result) => {
-    profile.push(result.firstName);
-    profile.push(result.lastName);
-    console.log(profile.join(' '));
-  }).catch(() => {
-    console.log('Signup system offline');
+  Promise.all([uploadPhoto(), createUser()]).then((result) => {
+    console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
   });
 }
