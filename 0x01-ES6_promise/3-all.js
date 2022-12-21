fa63875fe98632402e532758d487c0bb0534d4ff
Theocode12 <vchidubem9@gmail.com>
2022-12-21 23:40:32 +0100

    added catch

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,6 +4,6 @@ export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()]).then((result) => {
     console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
   }).catch(() => {
-    console.log('Signup system offline')
+    console.log('Signup system offline');
   });
 }
