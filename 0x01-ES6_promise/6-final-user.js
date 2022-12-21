ddc5cd42950abca340d0d01a0a28f48527e12c16
Theocode12 <vchidubem9@gmail.com>
2022-12-22 00:30:02 +0100

    function fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,6 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signup = signUpUser(firstName, lastName);
-  const upload = uploadPhoto(fileName);
-  return Promise.allSettled([signup, upload]).then((result) => result);
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((result) => result);
 }
