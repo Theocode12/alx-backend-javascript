ed6db00504004fc654ec33c83818d4065cd6a42a
Theocode12 <vchidubem9@gmail.com>
2022-12-22 00:10:40 +0100

    catch errors@

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,13 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const photo = await uploadPhoto();
-  const user = await createUser();
+  const errMessage = {
+    photo: null,
+    user: null,
+  };
+  const photo = await uploadPhoto().catch(errMessage);
+  const user = await createUser().catch(errMessage);
   return Promise.all([photo, user]).then((result) => ({
     photo: result[0],
     user: result[1],
-  })).catch(() => ({
-    photo: null,
-    user: null,
-  }));
+  })).catch(() => (errMessage));
 }
