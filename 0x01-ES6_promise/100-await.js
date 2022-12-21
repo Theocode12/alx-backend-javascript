ae215af4c32912477b78f4f404fbf559147f6989
Theocode12 <vchidubem9@gmail.com>
2022-12-22 00:18:31 +0100

    added catch

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -5,9 +5,8 @@ export default async function asyncUploadUser() {
     photo: null,
     user: null,
   };
-  const photo = await uploadPhoto().catch(errMessage);
-  const user = await createUser().catch(errMessage);
-  return Promise.all([photo, user]).then((result) => ({
+
+  return Promise.all([uploadPhoto(), createUser()]).then((result) => ({
     photo: result[0],
     user: result[1],
   })).catch(() => (errMessage));
