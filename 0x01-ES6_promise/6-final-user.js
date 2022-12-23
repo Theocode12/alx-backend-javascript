a75d7ee418d8e8bc09aca0d7bc69bfecba72e0a2
Theocode12 <vchidubem9@gmail.com>
2022-12-24 00:13:32 +0100

    return array fixed

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,17 @@ import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const result = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
-  return result;
+
+  const newResult = [];
+  for (const promise of result) {
+    if (promise.status === 'fulfilled') {
+      newResult.push(promise);
+    } else {
+      promise.value = `Error: ${promise.reason.message}`;
+      delete promise.reason;
+      newResult.push(promise);
+    }
+  }
+  console.log(newResult);
+  return newResult;
 }
