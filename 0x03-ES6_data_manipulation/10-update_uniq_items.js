5036d24e317392975aa6b5587e7f1c7e9bf75028
Theocode12 <vchidubem9@gmail.com>
2023-01-03 22:08:27 +0100

    update uniq items

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,8 +1,9 @@
 export default function updateUniqueItems(mymap) {
-  if (!mymap instanceof Map) throw Error('Cannot process')
-
-  mymap.forEach((value, key, thisArg) => {
-    if (value === 1) thisArg.set(key, 100);
-  })
-  return mymap
+  if (mymap instanceof Map) {
+    mymap.forEach((value, key, thisArg) => {
+      if (value === 1) thisArg.set(key, 100);
+    });
+    return mymap;
+  }
+  throw Error('Cannot process');
 }
