db8e75c94039defd14d733c3bab8d8683cee3e3a
Theocode12 <vchidubem9@gmail.com>
2023-01-03 20:38:38 +0100

    typedarrays

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,8 @@
+export default function createInt8TypedArray(length, position, value) {
+	if (position > length) throw new Error('Position outside range');
+      
+	const buffer = new ArrayBuffer(length);
+	const dataview = new DataView(buffer)
+  dataview.setInt8(2, value)
+  return dataview;
+}
