c9ec77c678bf8c6b3751fa06da3aacab36c16340
Theocode12 <vchidubem9@gmail.com>
2023-01-03 21:29:31 +0100

    typedarrays

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,11 @@
 export default function cleanSet(myset, startString) {
   const result = [];
   for (const key of myset) {
-    if (key.slice(0, startString.length) === startString && startString.length !== 0) {
-      result.push(key.slice(startString.length));
+    if (key.startsWith(startString) && startString.length !== 0) {
+      const subString = key.slice(startString.length);
+      if (subString && subString !== key) {
+        result.push(subString);
+      }
     }
   }
   return result.join('-');
