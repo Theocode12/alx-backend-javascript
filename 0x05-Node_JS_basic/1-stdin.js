bdb427fe51615911eeb622db9f856238feb3e308
Theocode12 <vchidubem9@gmail.com>
2023-02-21 17:35:48 +0100

    Basic node

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+const process = require('process');
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
+
+process.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
