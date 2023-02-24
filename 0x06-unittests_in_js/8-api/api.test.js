c39a6fb7ed7d35472560dd61a8cb8246264d92c1
Theocode12 <vchidubem9@gmail.com>
2023-02-24 23:33:21 +0100

    test_api

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -2,14 +2,29 @@ const request = require('request');
 const expect = require('chai').expect;
 
 describe('For index page of local server', function() {
-	it('should test for correct status code', function() {
-		request.get('http://localhost:7865', (err, res, body) =>{
-		expect(res.statusCode).to.equal(200);
+	it('should test for correct status code', function(done) {
+		request.get('http://localhost:7865/', (err, res, body) => {
+			expect(res.statusCode).to.equal(200);
+			done();
+		});
+
+	});
+	it('should test for correct body', function(done) {
+		request.get('http://localhost:7865/', (err, res, body) => {
+			expect(body).to.equal('Welcome to the payment system');
+			done();
 		});
 	});
-	it('should test for correct body', function() {
-		request.get('http://localhost:7865', (err, res, body) =>{
-		expect(body).to.equal('Welcome to the payment system');
+	it('should test for correct content type', function(done) {
+		request.get('http://localhost:7865/', (err, res, body) => {
+			expect(res.headers['content-type']).to.equal('text/plain; charset=utf-8');
+			done();
 		});
 	});
+	it ('should return 404 if wrong url', function(done) {
+		request.get('http://localhost:7865/test', (err, res, body) => {
+			expect(res.statusCode).to.equal(404);
+			done();
+		})
+	})
 });
