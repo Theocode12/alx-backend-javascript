2b29652ec11347df999d8dabf4165baa72ee223f
Theocode12 <vchidubem9@gmail.com>
2023-02-25 00:34:25 +0100

    api_test2

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -6,6 +6,10 @@ app.get('/', function(req, res) {
 	res.send('Welcome to the payment system')
 });
 
+app.get(/cart\/(\d+)/, function(req, res) {
+	res.send(`Payment methods for cart ${req.params['0']}`)
+});
+
 app.listen(7865, () => {
 	console.log('API available on localhost port 7865');
 })
