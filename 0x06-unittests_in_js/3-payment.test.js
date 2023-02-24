a744380617185d7e97816e9a292fd71640634fd7
Theocode12 <vchidubem9@gmail.com>
2023-02-24 16:30:18 +0100

    sinon spies

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -12,4 +12,32 @@ describe('sendPaymentRequestToApi', function (){
 		expect(spy.calledWithMatch('SUM')).to.be.true;
 		spy.restore();
 	})
+
+	it ('test if sendPaymentRequestToApi calculates total correctly', function() {
+		const spy = sinon.spy(Utils, 'calculateNumber');
+		sendPaymentRequestToApi(100, 20);
+		expect(spy.returnValues[0]).to.equal(120);
+		spy.restore();
+	});
+
+	it ('test if sendPaymentRequestToApi calculates total correctly with a rounded up', function() {
+		const spy = sinon.spy(Utils, 'calculateNumber');
+		sendPaymentRequestToApi(10.5, 20);
+		expect(spy.returnValues[0]).to.equal(31);
+		spy.restore();
+	});
+
+	it ('test if sendPaymentRequestToApi calculates total correctly with b rounded up', function() {
+		const spy = sinon.spy(Utils, 'calculateNumber');
+		sendPaymentRequestToApi(10, 18.5);
+		expect(spy.returnValues[0]).to.equal(29);
+		spy.restore();
+	});
+
+	it ('test if sendPaymentRequestToApi calculates total correctly with a rounded up', function() {
+		const spy = sinon.spy(Utils, 'calculateNumber');
+		sendPaymentRequestToApi(10.5, -20);
+		expect(spy.returnValues[0]).to.equal(-9);
+		spy.restore();
+	});
 });
