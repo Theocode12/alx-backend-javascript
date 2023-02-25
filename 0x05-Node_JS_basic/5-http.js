ce8be989bce4c90f7e72f08bf31e4f10f9314742
Theocode12 <vchidubem9@gmail.com>
2023-02-25 03:27:24 +0100

    http_module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -2,13 +2,17 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     const msg = 'This is the list of our students\n';
-    countStudents('database.csv').then((data) => {
+    countStudents(process.argv[2]).then((data) => {
       res.end(msg + data);
-    }).catch(() => res.statusCode(500).end());
+    }).catch(() => {
+      res.statusCode = 500;
+      res.end(`${msg}Cannot load the database`);
+    });
   }
 });
 
